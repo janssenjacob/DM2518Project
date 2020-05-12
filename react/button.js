2b53098d39b34782c9a914915fea1ea68a96d509
66Ronghua99 <ronghuali980303@gmail.com>
2020-05-12 12:28:49 +0800

    try react failed, continue on searchpage

---
+++ react/button.js
@@ -0,0 +1,12 @@
+const c = React.createElement
+
+function likeButton(){
+    // return (
+    // <ons-page>
+    //     <button>Click me</button>
+    // </ons-page>);
+    return (<ons-button>Please</ons-button>);
+}
+
+ReactDOM.render(likeButton(),document.getElementById('HomePage'))
+
